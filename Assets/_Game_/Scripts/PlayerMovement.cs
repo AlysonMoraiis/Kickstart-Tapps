08038f0710c9fc3c85f06822268cd4b2398b376d
AlysonMoraiis <alysonmorais35@hotmail.com>
2021-10-20 19:28:28 -0300

    Movimentos
    Organizei a unity.
    Coloquei o movimento do personagem.
    (falta tentar implementar o pathfinding)

---
+++ Assets/_Game_/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private Transform movePoint;
+    [SerializeField]
+    private LayerMask colliders;
+
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, colliders))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, colliders))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
+    }
+}
