c42270a3df74529bec8f04afb8d3a2a1763a2d30
Leonardo Dantas <leonardo_dantas@outlook.com.br>
2021-10-21 19:02:03 -0300

    Movimento por touch

--- Assets/_Game_/Scripts/PlayerMovement.cs
+++ Assets/_Game_/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,21 +11,71 @@ public class PlayerMovement : MonoBehaviour
     private Transform movePoint;
     [SerializeField]
     private LayerMask colliders;
+    public float runSpeed = 20.0f;
+    Rigidbody2D body;
+    public Vector2 velocity;
+
+
 
     void Start()
     {
         movePoint.parent = null;
+        body = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void FixedUpdate()
+    {
+        body.velocity = velocity;
     }
 
     void Update()
     {
+        MovementManager();
+    }
+
+    void MovementManager()
+    {
+        MovementKeys();
+        if (!IsMouseOverUi()) MovementTouch();
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+
+    }
+
+    void MovementTouch()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0f;
+            movePoint.position = worldPosition;
+            Debug.Log(worldPosition);
+        }
+    }
+
+    private bool IsMouseOverUi()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+
+    void MovementKeys()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        var vertical = Input.GetAxisRaw("Vertical");
+        velocity = new Vector2(horizontal, vertical);
+    }
+
+
+    /*void MovementKeys()
+    {
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, colliders))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, colliders))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
@@ -38,5 +89,7 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
-    }
+    }*/
+
 }
+
