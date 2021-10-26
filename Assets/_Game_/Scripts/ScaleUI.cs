99f23695c516bd0a69fbed583bdb7a543fc57a3e
Leonardo Dantas <leonardo_dantas@outlook.com.br>
2021-10-25 22:13:09 -0300

    Update Menu

---
+++ Assets/_Game_/Scripts/ScaleUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleUI : MonoBehaviour
+{
+    public float m_Range = 0.05f;
+    public float m_SmoothTime = 10.0f;
+    private Vector3 m_Scale;
+
+    private void Start()
+    {
+        m_Scale = transform.localScale;
+    }
+
+    private void LateUpdate()
+    {
+        transform.localScale = m_Scale + Vector3.one * Mathf.Sin(Time.time * m_SmoothTime) * m_Range;
+    }
+}
