6b627030ca803c92d5fe94bf12caf9d91b06301b
AlysonMoraiis <alysonmorais35@hotmail.com>
2021-10-27 13:59:02 -0300

    Random level 2

--- Assets/_Game_/Scripts/Scenary/LoadRandomScenes.cs
+++ Assets/_Game_/Scripts/Scenary/LoadRandomScenes.cs
@@ -6,22 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class LoadRandomScenes : MonoBehaviour
 {
-    private int currentLvl = 1;
+    private int currentLvl;
     private int randomLvl;
     public void LoadRandomScene()
     {
-        randomLvl = Random.Range(1,4);
-        if(randomLvl == currentLvl)
+        randomLvl = Random.Range(2,5);
+        while(randomLvl == currentLvl)
         {
             Debug.Log("Level repetido" + randomLvl);
-            LoadRandomScene();
+            randomLvl = Random.Range(2, 5);
+           // LoadRandomScene();
         }
-        else
-        {
         SceneManager.LoadScene(randomLvl);
         Debug.Log("Scene Loaded = " + randomLvl);
         currentLvl = randomLvl;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
